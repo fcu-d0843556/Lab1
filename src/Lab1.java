82dcaafa5bc3deb333e91ef1445788080cc57b60
user <user@EB12>
2020-09-29 09:36:15 +0800

    MY HW

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,7 +1,8 @@
 
 public class Lab1 {
 	public static void main(String[] args) {
-		System.out.println("Test");
-		System.out.println("2 ");
+		System.out.println("I'm ³¯¤l²W");
+		System.out.println("My NID is D0843556");
+		System.out.println("This is lab 1 HW");
 	}
 }
